5dfd9a7a8c7a830a59cb63470c4b4c5bb72da395
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-13 11:39:43 +0500

    Density homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            double m;
+            double v;
+            System.out.println("Привет я помогу тебе найти плотность, введи массу тела");
+
+            m = scanner.nextDouble();
+            System.out.println("Отлично масса тела" + m + " теперь введи объём тела");
+            v = scanner.nextDouble();
+            double p = m / v;
+            System.out.println("Плотность тела:" + p);
+        }catch (Exception ex){
+            System.out.println("Ты ввёл что-то не так");
+        }
+
     }
 }
